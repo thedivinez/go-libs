6fbb5238594df74c56c5f8e316a1038d88be69e2
thedivinez <dzbusiness01@gmail.com>
2024-11-29 12:53:02 +0200

    add event message type

---
+++ utils/types.go
@@ -0,0 +1,18 @@
+package utils
+
+import "encoding/json"
+
+type EventMessage struct {
+	Message any
+	Room    string
+	Event   string
+	Service string
+}
+
+func (ev EventMessage) MarshalBinary() ([]byte, error) {
+	return json.Marshal(ev)
+}
+
+func (ev *EventMessage) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, ev)
+}
