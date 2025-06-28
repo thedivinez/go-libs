d832d767d96492dee4a1a513355353c55c791a24
thedivinez <dzbusiness01@gmail.com>
2025-06-28 11:47:49 +0200

    fix type for rps bet

--- rps/types.go
+++ rps/types.go
@@ -2,10 +2,10 @@ package rps
 
 import "encoding/json"
 
-func (s *Bet) MarshalBinary() ([]byte, error) {
+func (s *RPSBet) MarshalBinary() ([]byte, error) {
 	return json.Marshal(s)
 }
 
-func (s *Bet) UnmarshalBinary(data []byte) error {
+func (s *RPSBet) UnmarshalBinary(data []byte) error {
 	return json.Unmarshal(data, s)
 }
