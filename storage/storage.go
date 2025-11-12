11f92d40d25c76dbc5a2eeb6c01d24fba57adb08
thedivinez <dzbusiness01@gmail.com>
2025-11-12 16:02:28 +0200

    fix insert many

--- storage/storage.go
+++ storage/storage.go
@@ -9,7 +9,7 @@ type Database interface {
 	DeleteMany(string, any) error
 	Count(string, any) (int64, error)
 	InsertOne(string, any) (string, error)
-	InsertMany(collection string, data ...any) (err error)
+	InsertMany(collection string, data []interface{}) (err error)
 	GenerateID(collection string, letters int, digits int) *ID
 	Aggregate(collection string, filter any, results any) error
 	UpdateOne(collection string, filter any, values any, opts ...*options.UpdateOptions) error
